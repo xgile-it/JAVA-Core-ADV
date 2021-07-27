553b0f323ac279eba29167196f5e3deb1c433a0a
Vishal Singh <87254573+VishalSingh33@users.noreply.github.com>
2021-07-27 20:40:46 +0530

    Add files via upload
    Assignments Xgile

--- Decimalcomparsion.java
+++ Decimalcomparsion.java
@@ -2,35 +2,30 @@ import java.util.Scanner;
 
 public class Decimalcomparsion {
 
-	public Decimalcomparsion() {
-		
-	}
+    public Decimalcomparsion() {
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	//taking input of float type from user	
-     Scanner sc = new Scanner(System.in);
-     System.out.println("Enter the double value");
-     double num = sc.nextDouble();
-     
-     //changing float value to string 
-     String doubleAsString = String.valueOf(num);
-     //values after decimal point
-     int indexOfDecimal = doubleAsString.indexOf(".");
-     
-     System.out.println("Double Number: " + num);
-     System.out.println("Integer Part: " + doubleAsString.substring(0, indexOfDecimal));
-     System.out.println("Decimal Part: " + doubleAsString.substring(indexOfDecimal+1));
+    public static void main(String[] args) {
+        //taking input of float type from user	
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the double value");
+        double num = sc.nextDouble();
+
+        //changing float value to string 
+        String doubleAsString = String.valueOf(num);
+        //values after decimal point
+        int indexOfDecimal = doubleAsString.indexOf(".");
+
+        System.out.println("Double Number: " + num);
+        System.out.println("Integer Part: " + doubleAsString.substring(0, indexOfDecimal));
+        System.out.println("Decimal Part: " + doubleAsString.substring(indexOfDecimal + 1));
      //equal or not
-     
-     if(doubleAsString.substring(indexOfDecimal+1).equals(doubleAsString.substring(0, indexOfDecimal)))
-     {
-    	 
-         System.out.println("Equal");
-	}
-	else
-	{
-	System.out.println("Not Equal");	
-	}
-	}
+
+        if (doubleAsString.substring(indexOfDecimal + 1).equals(doubleAsString.substring(0, indexOfDecimal))) {
+
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
 }
